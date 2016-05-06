b6eb7d77f191f91b85cdd6ae4f83664376ccd1cc
Moiz Qureshi <moizq@calpak-usa.com>
2016-05-05 18:13:14 -0700

    Removed Partner Class references

--- app/src/main/java/com/example/moizqureshi/coupletones/User.java
+++ app/src/main/java/com/example/moizqureshi/coupletones/User.java
@@ -9,13 +9,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 public class User {
     GoogleSignInAccount gAccount;
     String username;
-    Partner partner;
     Locations locations;
 
     User( GoogleSignInAccount argAcc ) {
         gAccount = argAcc;
         username = argAcc.getDisplayName();
-        partner = null;
         locations = new Locations( );
     }
 
@@ -27,32 +25,12 @@ public class User {
         return username;
     }
 
-    public Partner getPartner( ) {
-        return partner;
-    }
-
     public Locations getLocations( ) {
         return locations;
     }
 
     public void setUsername( String newUsername ) {
         username = newUsername;
-    }
-
-    public void setPartner( Partner newPartner) {
-        partner = newPartner;
-    }
-
-    public boolean removePartner( ) {
-        if( !hasPartner( ) )
-            return false;
-
-        partner = null;
-        return true;
-    }
-
-    public boolean hasPartner( ) {
-        return !(partner == null);
     }
 
     public boolean saveInfo( ) {
