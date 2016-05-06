5e23ac33d1325d1e4c00db817b46ae4df25aef78
Moiz Qureshi <moizq@calpak-usa.com>
2016-05-06 12:11:11 -0700

    Finished implementing add/delete partner and pairing of partner_id's

--- app/src/main/java/com/example/moizqureshi/coupletones/User.java
+++ app/src/main/java/com/example/moizqureshi/coupletones/User.java
@@ -11,11 +11,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 public class User {
     private String email;
     private String partnerEmail;
+    private String partnerId;
     private Locations locations;
 
     public User( ) {
         email = null;
         partnerEmail = new String("--");
+        partnerId = null;
         locations = new Locations( );
     }
 
@@ -33,12 +35,29 @@ public class User {
         return partnerEmail;
     }
 
+    public String getPartnerId() {
+        return partnerId;
+    }
+
     public Locations getLocations( ) {
         return locations;
     }
 
     public void setPartnerEmail( String newPartnerEmail ) {
+        if (newPartnerEmail == null) {
+            partnerEmail ="--";
+            return;
+        }
         partnerEmail = newPartnerEmail;
+    }
+
+    public void setPartnerId(String newPartnerId) {
+        if (newPartnerId == null){
+            partnerId = "--";
+            return;
+        }
+        partnerId = newPartnerId;
+
     }
 
     public boolean removePartner( ) {
@@ -46,6 +65,7 @@ public class User {
             return false;
 
         partnerEmail = new String("--");
+        partnerId = new String("--");
         return true;
     }
 
