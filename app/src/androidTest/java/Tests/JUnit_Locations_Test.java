2589a4263acc4c444a7a89d4bc913d03206ee31b
Alina <agoldner@ucsd.edu>
2016-05-05 17:50:48 -0700

    Locations JUnit Test partially done.

---
+++ app/src/androidTest/java/Tests/JUnit_Locations_Test.java
@@ -0,0 +1,69 @@
+package Tests;
+
+import com.example.moizqureshi.coupletones.FavLocation;
+import com.example.moizqureshi.coupletones.Locations;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alina on 5/5/2016.
+ */
+public class JUnit_Locations_Test  {
+
+    Locations testLocations;
+
+    @Before
+    public void setUp(){
+        Locations testOne;
+        Locations testTwo;
+        Locations testThree;
+        int size = 5;
+        ArrayList<FavLocation> testThreeParam = new ArrayList<>(size);
+
+    }
+
+    @Test
+    public void getFavLocation(){
+
+    }
+
+    @Test
+    public void testAdd(){
+
+    }
+
+    @Test
+    public void testRemoveByIndex(){
+
+    }
+
+    @Test
+    public void testRemoveByLocation(){
+
+    }
+
+    @Test
+    public void testRemoveByName(){
+
+    }
+
+
+    @Test
+    public void testSearchIdx(){
+
+    }
+
+    @Test
+    public void testSearchForLocationByLocation(){
+
+    }
+
+    @Test
+    public void testSearchForLocationByName(){
+
+    }
+
+}
