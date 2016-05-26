71e75d288c78514140f3b35d423df19799f525bb
Moiz Qureshi <moizq@calpak-usa.com>
2016-05-25 22:00:20 -0700

    Implemented background Notificaiton receiver to play sounds

--- app/src/main/java/com/example/moizqureshi/coupletones/Sounds.java
+++ app/src/main/java/com/example/moizqureshi/coupletones/Sounds.java
@@ -1,29 +1,52 @@
 package com.example.moizqureshi.coupletones;
 
 import android.app.Application;
+import android.content.Context;
 import android.media.MediaPlayer;
+import android.util.Log;
 
 /**
  * Created by moizqureshi on 5/25/16.
  */
-public class Sounds extends Application {
+public class Sounds {
 
+    private Context context;
     public MediaPlayer mp1, mp2;
 
     private int[] defaultSound = { R.raw.space1, R.raw.space2};
     private int[] soundList = { R.raw.birdy, R.raw.bottle, R.raw.elastic, R.raw.enigmatic, R.raw.ice,
             R.raw.mallet, R.raw.softbell, R.raw.spaceball, R.raw.toy, R.raw.wood };
 
-    public Sounds() {
+    public Sounds(Context context) {
+        this.context = context;
         mp1 = new MediaPlayer();
         mp2 = new MediaPlayer();
     }
 
     public void setPlayer(int def, int idx) {
-        mp1 = MediaPlayer.create(this, soundList[def]);
+        mp1 = MediaPlayer.create(context, defaultSound[def]);
+        mp2 = MediaPlayer.create(context, soundList[idx]);
     }
 
-
+    public void playSounds() {
+        Log.d("Test", "in playSounds");
+        mp1.start();
+        mp1.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.pause();
+                mp.stop();
+                mp2.start();
+                mp2.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        mp.pause();
+                        mp.stop();
+                    }
+                });
+            }
+        });
+    }
 
 
 }
