4752e44b700013401b988b9fcf2be1014cf32505
Moiz Qureshi <moizq@calpak-usa.com>
2016-05-25 22:00:20 -0700

    Implemented background Notificaiton receiver to play sounds

---
+++ app/src/main/java/com/example/moizqureshi/coupletones/OneSignalBackgroundDataReceiver.java
@@ -0,0 +1,35 @@
+package com.example.moizqureshi.coupletones;
+
+import android.support.v4.content.WakefulBroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+/**
+ * Created by moizqureshi on 5/25/16.
+ */
+
+
+
+public class OneSignalBackgroundDataReceiver extends WakefulBroadcastReceiver {
+    public void onReceive(Context context, Intent intent) {
+        Bundle dataBundle = intent.getBundleExtra("data");
+        Sounds sound = new Sounds(context);
+        sound.setPlayer(1,4);
+        sound.playSounds();
+        try {
+            //Log.i("OneSignalExample", "NotificationTable content: " + dataBundle.getString("alert"));
+            Log.i("OneSignalExample", "NotificationTable title: " + dataBundle.getString("title"));
+            Log.i("OneSignalExample", "Is Your App Active: " + dataBundle.getBoolean("isActive"));
+            Log.i("OneSignalExample", "data addt: " + dataBundle.getString("custom"));
+            JSONObject customJSON = new JSONObject(dataBundle.getString("custom"));
+            if (customJSON.has("a"))
+                Log.i("OneSignalExample", "additionalData:key_a: " + customJSON.getJSONObject("a").getString("a"));
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/example/moizqureshi/coupletones/ourApplication.java
+++ app/src/main/java/com/example/moizqureshi/coupletones/ourApplication.java
@@ -2,6 +2,7 @@ package com.example.moizqureshi.coupletones;
 
 import android.app.Application;
 import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,6 +17,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.maps.MapFragment;
+import com.onesignal.NotificationExtenderService;
+import com.onesignal.OSNotificationPayload;
 import com.onesignal.OneSignal;
 import com.parse.Parse;
 import android.widget.TextView;
@@ -49,61 +52,72 @@ public class ourApplication extends Application {
 
         OneSignal.startInit(this)
                 .setAutoPromptLocation(true)
-                .setNotificationOpenedHandler(new ExampleNotificationOpenedHandler())
+//                .setNotificationOpenedHandler(new ExampleNotificationOpenedHandler())
                 .init();
         OneSignal.enableInAppAlertNotification(true);
         OneSignal.enableNotificationsWhenActive(false);
+        OneSignal.enableSound(false);
+        OneSignal.enableVibrate(false);
 
     }
 
-    private class ExampleNotificationOpenedHandler implements OneSignal.NotificationOpenedHandler {
-        /**
-         * Callback to implement in your app to handle when a notification is opened from the Android status bar or
-         * a new one comes in while the app is running.
-         * This method is located in this Application class as an example, you may have any class you wish implement NotificationOpenedHandler and define this method.
-         *
-         * @param message        The message string the user seen/should see in the Android status bar.
-         * @param additionalData The additionalData key value pair section you entered in on onesignal.com.
-         * @param isActive       Was the app in the foreground when the notification was received.
-         */
-        @Override
-        public void notificationOpened(String message, JSONObject additionalData, boolean isActive) {
-            String additionalMessage = "";
-            try {
-                if (additionalData != null) {
-                    Log.d("Notification from: ", additionalData.getString("fromID"));
-                    if (additionalData.has("actionSelected")) {
-                        if(additionalData.getString("actionSelected").toString().equals("acceptBtn")) {
-                            currUser.setPartnerEmail(additionalData.getString("email"));
-                            manager.updatePartnerEmail(currUser);
-                            MainActivity.setPartnerTxt(currUser.getPartnerEmail());
-                            manager.sendPairSuccess(additionalData.getString("fromID"), additionalData.getString("email"));
+//    private class ExampleNotificationOpenedHandler implements OneSignal.NotificationOpenedHandler {
+//        /**
+//         * Callback to implement in your app to handle when a notification is opened from the Android status bar or
+//         * a new one comes in while the app is running.
+//         * This method is located in this Application class as an example, you may have any class you wish implement NotificationOpenedHandler and define this method.
+//         *
+//         * @param message        The message string the user seen/should see in the Android status bar.
+//         * @param additionalData The additionalData key value pair section you entered in on onesignal.com.
+//         * @param isActive       Was the app in the foreground when the notification was received.
+//         */
+//        @Override
+//        public void notificationOpened(String message, JSONObject additionalData, boolean isActive) {
+//            String additionalMessage = "";
+//
+//            try {
+//                if (additionalData != null) {
+//                    Log.d("Notification from: ", additionalData.getString("fromID"));
+//                    if (additionalData.has("actionSelected")) {
+//                        if(additionalData.getString("actionSelected").toString().equals("acceptBtn")) {
+//                            currUser.setPartnerEmail(additionalData.getString("email"));
+//                            manager.updatePartnerEmail(currUser);
+//                            MainActivity.setPartnerTxt(currUser.getPartnerEmail());
+//                            manager.sendPairSuccess(additionalData.getString("fromID"), additionalData.getString("email"));
+//
+//                            additionalMessage += "Pressed ButtonID: " + additionalData.getString("actionSelected");
+//                            Log.d("Test", "Accepted pair request: Partner is: " + currUser.getPartnerEmail());
+//                        } else {
+//                            manager.sendPairFail(additionalData.getString("fromID"), additionalData.getString("email"));
+//                        }
+//                    }
+//
+//                    if (additionalData.has("pairSuccess")) {
+//                        if(additionalData.getString("pairSuccess").toString().equals("true")) {
+//                            currUser.setPartnerEmail(additionalData.getString("email"));
+//                            manager.updatePartnerEmail(currUser);
+//                            MainActivity.setPartnerTxt(currUser.getPartnerEmail());
+//                            Log.d("Test", "Pairing was successful with " + currUser.getPartnerEmail());
+//                        } else {
+//                            Log.d("Test", "Pairing declined by " + additionalData.getString("email"));
+//                        }
+//                    }
+//
+//                    additionalMessage = message + "\nFull additionalData:\n" + additionalData.toString();
+//
+//                }
+//                Log.d("OneSignalExample", "message:\n" + message + "\nadditionalMessage:\n" + additionalMessage);
+//            } catch (Throwable t) {
+//                t.printStackTrace();
+//            }
+//        }
+////
+//        }
+//
+//    }
 
-                            additionalMessage += "Pressed ButtonID: " + additionalData.getString("actionSelected");
-                            Log.d("Test", "Accepted pair request: Partner is: " + currUser.getPartnerEmail());
-                        } else {
-                            manager.sendPairFail(additionalData.getString("fromID"), additionalData.getString("email"));
-                        }
-                    }
 
-                    if (additionalData.has("pairSuccess")) {
-                        if(additionalData.getString("pairSuccess").toString().equals("true")) {
-                            currUser.setPartnerEmail(additionalData.getString("email"));
-                            manager.updatePartnerEmail(currUser);
-                            MainActivity.setPartnerTxt(currUser.getPartnerEmail());
-                            Log.d("Test", "Pairing was successful with " + currUser.getPartnerEmail());
-                        } else {
-                            Log.d("Test", "Pairing declined by " + additionalData.getString("email"));
-                        }
-                    }
 
-                    additionalMessage = message + "\nFull additionalData:\n" + additionalData.toString();
-                }
 
-                Log.d("OneSignalExample", "message:\n" + message + "\nadditionalMessage:\n" + additionalMessage);
-            } catch (Throwable t) {
-                t.printStackTrace();
-            }
-        }
-    }
+
 }
