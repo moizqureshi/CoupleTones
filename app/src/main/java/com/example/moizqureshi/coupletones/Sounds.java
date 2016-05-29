d2d2bccd171d594424b81a07e5188e757d814eb2
Moiz Qureshi <moizq@calpak-usa.com>
2016-05-28 22:08:03 -0700

    Fixed sounds class to just play one sound

--- app/src/main/java/com/example/moizqureshi/coupletones/Sounds.java
+++ app/src/main/java/com/example/moizqureshi/coupletones/Sounds.java
@@ -11,39 +11,27 @@ import android.util.Log;
 public class Sounds {
 
     private Context context;
-    public MediaPlayer mp1, mp2;
+    public MediaPlayer mp1;
 
-    private int[] defaultSound = { R.raw.space1, R.raw.space2};
-    private int[] soundList = { R.raw.birdy, R.raw.bottle, R.raw.elastic, R.raw.enigmatic, R.raw.ice,
-            R.raw.mallet, R.raw.softbell, R.raw.spaceball, R.raw.toy, R.raw.wood };
+    private int[] sounds = { R.raw.birdy, R.raw.bottle, R.raw.elastic, R.raw.enigmatic, R.raw.ice,
+            R.raw.mallet, R.raw.softbell, R.raw.spaceball, R.raw.toy, R.raw.wood, R.raw.space1, R.raw.space2 };
 
     public Sounds(Context context) {
         this.context = context;
         mp1 = new MediaPlayer();
-        mp2 = new MediaPlayer();
     }
 
-    public void setPlayer(int def, int idx) {
-        mp1 = MediaPlayer.create(context, defaultSound[def]);
-        mp2 = MediaPlayer.create(context, soundList[idx]);
+    public void setPlayer(int index) {
+        mp1 = MediaPlayer.create(context, sounds[index]);
     }
 
-    public void playSounds() {
-        Log.d("Test", "in playSounds");
+    public void playSound() {
         mp1.start();
         mp1.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
                 mp.pause();
                 mp.stop();
-                mp2.start();
-                mp2.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mp) {
-                        mp.pause();
-                        mp.stop();
-                    }
-                });
             }
         });
     }
